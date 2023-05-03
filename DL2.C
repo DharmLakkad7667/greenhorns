2e4598658e8b7cf7a31de7650119987f19ab629f
DharmLakkad7667 <132447689+DharmLakkad7667@users.noreply.github.com>
2023-05-03 15:24:25 +0530

    Add files via upload

---
+++ DL2.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+ clrscr();
+  printf("\n\t*\t*\t*\t\*\t*\t*\t*\n");
+  printf("\n\t\t\t\t\t\t\t\n");
+  printf("\n*\t*\t*\t*\t*\t*\t*\t*\t\n");
+  printf("\n*\t\t*\t\t\t\t\t*\n");
+  printf("\n*\t\t*\t\t*\t*
